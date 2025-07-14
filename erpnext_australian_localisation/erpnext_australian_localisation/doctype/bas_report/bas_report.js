aa79cf7b6bf89c92f51cf197d03bc44bcca894dd
Jebajebas <jeba.j@arus.co.in>
2025-07-14 17:51:51 +0530

    chore : add msgprint in change of date in bas report

--- erpnext_australian_localisation/erpnext_australian_localisation/doctype/bas_report/bas_report.js
+++ erpnext_australian_localisation/erpnext_australian_localisation/doctype/bas_report/bas_report.js
@@ -5,26 +5,25 @@ let reporting_period = ""
 
 frappe.ui.form.on("BAS Report", {
 	refresh(frm) {
-	
+
 		if (frm.is_new()) {
 		}
 		else {
 			frm.trigger("update_reporting_period")
-		}
-
-		frm.add_custom_button(__("Update BAS Data"), () => {
-			frappe.call({
-				method: "erpnext_australian_localisation.erpnext_australian_localisation.doctype.bas_report.bas_report.get_gst",
-				args: {
-					company: frm.doc.company,
-					start_date: frm.doc.start_date,
-					end_date : frm.doc.end_date
-				},
-				callback: function (data) {
-					console.log(data.message)
-				}
+			frm.add_custom_button(__("Update BAS Data"), () => {
+				frappe.call({
+					method: "erpnext_australian_localisation.erpnext_australian_localisation.doctype.bas_report.bas_report.get_gst",
+					args: {
+						company: frm.doc.company,
+						start_date: frm.doc.start_date,
+						end_date: frm.doc.end_date
+					},
+					callback: function (data) {
+						console.log(data.message)
+					}
+				})
 			})
-		})
+		}
 	},
 	company(frm) {
 		frm.trigger("update_reporting_period")
@@ -35,26 +34,40 @@ frappe.ui.form.on("BAS Report", {
 		frm.trigger("update_end_date")
 	},
 
-	update_end_date(frm) {
+	update_end_date: async function (frm) {
 		if (frm.doc.start_date && frm.doc.company) {
-			if (! reporting_period ) {
+			if (!reporting_period) {
 				frappe.throw("Please set reporting period in <a href='/app/australian-localisation-settings/Australian Localisation Settings' > ERPNext Australian Settings </a>")
 			}
-			else if (reporting_period === "Monthly") {
-				frm.set_value( "start_date", moment(frm.doc.start_date).startOf("month").format() )
-				frm.set_value( "end_date", moment(frm.doc.start_date).endOf("month").format() )
-			}
-			else if (reporting_period === "Quarterly") {
-			frappe.call({
-					method: "erpnext_australian_localisation.erpnext_australian_localisation.doctype.bas_report.bas_report.get_quaterly_start_end_date",
-					args: {
-						"start_date": frm.doc.start_date
-					},
-					callback: function (data) {
-						frm.set_value("start_date", data.message[0])
-						frm.set_value("end_date", data.message[1])
-					}
-				})
+			else if (reporting_period) {
+				if (reporting_period === "Monthly") {
+					await frm.set_value("start_date", moment(frm.doc.start_date).startOf("month").format())
+						.then((e) => {
+							if (e === null) {
+								frappe.msgprint("Start date is changed to " + frm.doc.start_date + " to keep it in line with the " + reporting_period + " BAS setup")
+
+							}
+						})
+					frm.set_value("end_date", moment(frm.doc.start_date).endOf("month").format())
+				}
+				else if (reporting_period === "Quarterly") {
+					frappe.call({
+						method: "erpnext_australian_localisation.erpnext_australian_localisation.doctype.bas_report.bas_report.get_quaterly_start_end_date",
+						args: {
+							"start_date": frm.doc.start_date
+						},
+						callback: async function (data) {
+							await frm.set_value("start_date", data.message[0])
+								.then((e) => {
+									if (e === null) {
+										frappe.msgprint("Start date is changed to " + frm.doc.start_date + " to keep it in line with the " + reporting_period + " BAS setup")
+
+									}
+								})
+							frm.set_value("end_date", data.message[1])
+						}
+					})
+				}
 			}
 		}
 		else {
@@ -64,12 +77,12 @@ frappe.ui.form.on("BAS Report", {
 
 	update_reporting_period(frm) {
 		let brp = australian_localisation_settings.bas_reporting_period
-		for (let i = 0; i < brp.length; i++){
+		for (let i = 0; i < brp.length; i++) {
 			if (brp[i].company === frm.doc.company) {
 				reporting_period = brp[i].reporting_period;
 				break;
 			}
 		}
 	}
-	
+
 });
