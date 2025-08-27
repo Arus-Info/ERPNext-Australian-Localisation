34b9b105f5267087831fef0f50424fb8f46b3e32
Jebajebas <jeba.j@arus.co.in>
2025-08-27 16:21:41 +0530

    fix: eslint error

--- erpnext_australian_localisation/public/js/bank_account.js
+++ erpnext_australian_localisation/public/js/bank_account.js
@@ -2,8 +2,8 @@ frappe.ui.form.on("Bank Account", {
 	refresh(frm) {
 		frappe.db.get_value("Company", frm.doc.company, "country").then((data) => {
 			if (data.message.country === "Australia") {
-				// validate_bank_details(frm);
-				// validate_apca_number(frm);
+				validate_bank_details(frm); // eslint-disable-line no-undef
+				validate_apca_number(frm); // eslint-disable-line no-undef
 			}
 		});
 	},
