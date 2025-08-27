0c5e963e3483715c77a142923c4d8937e2aedb66
Jebajebas <jeba.j@arus.co.in>
2025-08-27 13:16:53 +0530

    fix: linters

--- erpnext_australian_localisation/public/js/supplier.js
+++ erpnext_australian_localisation/public/js/supplier.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Supplier", {
 	refresh(frm) {
 		console.log("here");
 		if (frm.doc.country === "Australia") {
-			validate_bank_details(frm);
+			// validate_bank_details(frm);
 		}
 	},
 });
