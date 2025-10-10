111f018fd1b592f04d45ad84baa5fd024291391c
Jeba Jebas <jeba.j@arus.co.in>
2025-10-10 11:52:18 +0530

    feat: print format for simpler bas report (#97)

--- erpnext_australian_localisation/patches/update_file_format_in_bank_account.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-
-def execute():
-	frappe.db.set_value(
-		"Custom Field",
-		{"dt": "Bank Account", "fieldname": "file_format"},
-		{"options": "-None-\nABA", "default": "-None-"},
-	)
