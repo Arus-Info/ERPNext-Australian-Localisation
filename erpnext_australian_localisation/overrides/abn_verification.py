e7eb81fc8049b4f1722d3e40cfa017a47659316c
Thanish855 <thanisha.p@arus.co.in>
2026-02-09 13:19:11 +0530

    fix:issues

--- erpnext_australian_localisation/overrides/abn_verification.py
+++ erpnext_australian_localisation/overrides/abn_verification.py
@@ -8,7 +8,7 @@ from frappe import _
 @frappe.whitelist()
 def fetch_and_update_abn(tax_id):
 	settings = frappe.get_single("AU Localisation Settings")
-	# no check in au localisation settings no api calls
+	# no check in au localisation settings no api call
 	if not settings.is_verify_abn:
 		return
 
