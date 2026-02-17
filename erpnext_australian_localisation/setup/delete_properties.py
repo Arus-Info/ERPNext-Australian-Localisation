5fa1feaaa56c0a36f22a6b915af9cb7d78be315c
Jeba Jebas <jeba.j@arus.co.in>
2026-02-17 17:43:29 +0530

    feat: au bank import format (#162)

--- erpnext_australian_localisation/setup/delete_properties.py
+++ erpnext_australian_localisation/setup/delete_properties.py
@@ -1,12 +1,14 @@
 import frappe
 
 from erpnext_australian_localisation.setup.custom_fields import (
+	ABN_GUID_FIELDS,
+	BAI2_FIELDS,
 	CUSTOM_FIELDS,
 	CUSTOM_FIELDS_FOR_BANK_FILE,
 	EMPLOYEE_BANK_DETAILS,
 	HRMS_CUSTOM_FIELDS,
 )
-from erpnext_australian_localisation.setup.property_setters import PROPERTIES
+from erpnext_australian_localisation.setup.property_setters import ABN_PROPERTIES, PROPERTIES
 
 
 def delete_custom_field(custom_fields):
@@ -29,13 +31,13 @@ def delete_custom_field(custom_fields):
 			frappe.clear_cache(doctype=doctype)
 
 
-def delete_property_setter():
+def delete_property_setter(properties):
 	field_map = {
 		"doctype": "doc_type",
 		"fieldname": "field_name",
 	}
 
-	for property_setter in PROPERTIES:
+	for property_setter in properties:
 		for key, fieldname in field_map.items():
 			if key in property_setter:
 				property_setter[fieldname] = property_setter.pop(key)
@@ -46,9 +48,12 @@ def delete_property_setter():
 def remove_setup():
 	delete_custom_field(CUSTOM_FIELDS)
 	delete_custom_field(CUSTOM_FIELDS_FOR_BANK_FILE)
+	delete_custom_field(BAI2_FIELDS)
+	delete_custom_field(ABN_GUID_FIELDS)
 	if "hrms" in frappe.get_installed_apps():
 		delete_hrms_custom_fields()
-	delete_property_setter()
+	delete_property_setter(PROPERTIES)
+	delete_property_setter(ABN_PROPERTIES)
 
 
 def delete_hrms_custom_fields():
