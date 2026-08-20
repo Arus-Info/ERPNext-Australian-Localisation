912317cda446bb5f48ecd58e582e85056c0a7577
user <thanisha.p@arus.co.in>
2026-08-20 09:36:58 +0530

    fix:date in email template

--- erpnext_australian_localisation/erpnext_australian_localisation/doctype/au_localisation_settings/au_localisation_settings.py
+++ erpnext_australian_localisation/erpnext_australian_localisation/doctype/au_localisation_settings/au_localisation_settings.py
@@ -16,3 +16,45 @@ def is_draft(company):
 	if bas_report:
 		return True
 	return False
+
+
+def get_au_email_templates():
+	from erpnext_australian_localisation.setup.install_fixtures import get_default_email_templates
+
+	settings = frappe.get_single("AU Localisation Settings")
+
+	templates = [row["name"] for row in get_default_email_templates()]
+
+	templates += [
+		settings.remittance_advice_template,
+		settings.payment_receipt_template,
+	]
+
+	return list(set(filter(None, templates)))
+
+@frappe.whitelist()
+def get_disabled_email_templates():
+	"""Templates that are not yet ticked as `enabled` for CRM."""
+	if "crm" not in frappe.get_installed_apps():
+		return []
+
+	return frappe.get_all(
+		"Email Template",
+		filters={"name": ("in", get_au_email_templates()), "enabled": 0},
+		pluck="name",
+	)
+
+
+@frappe.whitelist()
+def enable_email_templates():
+
+	if "crm" not in frappe.get_installed_apps():
+		return []
+
+	frappe.has_permission("Email Template", "write", throw=True)
+
+	enabled = get_disabled_email_templates()
+	for template in enabled:
+		frappe.db.set_value("Email Template", template, "enabled", 1)
+
+	return enabled
