d9b7c5555d2d070553507d957ba34a70809b39f3
Thanish855 <thanisha.p@arus.co.in>
2026-08-28 09:15:09 +0530

    fix: date in email template (#188)

--- erpnext_australian_localisation/erpnext_australian_localisation/doctype/au_localisation_settings/au_localisation_settings.py
+++ erpnext_australian_localisation/erpnext_australian_localisation/doctype/au_localisation_settings/au_localisation_settings.py
@@ -16,3 +16,27 @@ def is_draft(company):
 	if bas_report:
 		return True
 	return False
+
+
+def get_au_email_templates():
+	from erpnext_australian_localisation.setup.install_fixtures import get_default_email_templates
+
+	return [record["name"] for record in get_default_email_templates()]
+
+
+@frappe.whitelist()
+def get_disabled_email_templates():
+	return frappe.get_all(
+		"Email Template",
+		filters={"name": ("in", get_au_email_templates()), "enabled": 0},
+		pluck="name",
+	)
+
+
+@frappe.whitelist()
+def enable_email_templates():
+	enabled = get_disabled_email_templates()
+	for template in enabled:
+		frappe.db.set_value("Email Template", template, "enabled", 1)
+
+	return enabled
