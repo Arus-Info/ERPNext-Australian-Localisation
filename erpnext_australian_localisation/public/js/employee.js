75d23a14c321a582616c0da643b7b5c7f6d86109
user <thanisha.p@arus.co.in>
2026-09-07 12:04:50 +0530

    fix: refresh issue

--- erpnext_australian_localisation/public/js/employee.js
+++ erpnext_australian_localisation/public/js/employee.js
@@ -1,15 +1,23 @@
 frappe.ui.form.on("Employee", {
-	bank_ac_no(frm) {
-		const account_number = frm.doc.bank_ac_no || "";
+	refresh(frm) {
+		set_bank_account_description(frm);
+	},
 
-		if (account_number.length === 10) {
-			frm.set_df_property(
-				"bank_ac_no",
-				"description",
-				__("10 digit bank account numbers will not be supported for abn file generation.")
-			);
-		} else {
-			frm.set_df_property("bank_ac_no", "description", "");
-		}
+	bank_ac_no(frm) {
+		set_bank_account_description(frm);
 	}
 });
+
+function set_bank_account_description(frm) {
+	const account_number = frm.doc.bank_ac_no || "";
+
+	if (account_number.length === 10) {
+		frm.set_df_property(
+			"bank_ac_no",
+			"description",
+			__("10 digit bank account numbers will not be supported for abn file generation.")
+		);
+	} else {
+		frm.set_df_property("bank_ac_no", "description", "");
+	}
+}
