35ccc78f7d82073a9897af0b75165c3d6b46d6d0
user <thanisha.p@arus.co.in>
2026-09-10 11:15:23 +0530

    fix: description in employee

--- erpnext_australian_localisation/public/js/employee.js
+++ erpnext_australian_localisation/public/js/employee.js
@@ -3,21 +3,21 @@ frappe.ui.form.on("Employee", {
 		set_bank_account_description(frm);
 	},
 
-	bank_ac_no(frm) {
+	bank_account_no(frm) {
 		set_bank_account_description(frm);
 	}
 });
 
 function set_bank_account_description(frm) {
-	const account_number = frm.doc.bank_ac_no || "";
+	const account_number = frm.doc.bank_account_no || "";
 
 	if (account_number.length === 10) {
 		frm.set_df_property(
-			"bank_ac_no",
+			"bank_account_no",
 			"description",
 			__("10 digit bank account numbers will not be supported for abn file generation.")
 		);
 	} else {
-		frm.set_df_property("bank_ac_no", "description", "");
+		frm.set_df_property("bank_account_no", "description", "");
 	}
 }
