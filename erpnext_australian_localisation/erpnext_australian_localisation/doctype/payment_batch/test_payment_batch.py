159686b503df3a440330d23dc05b51c290658dd1
Jebajebas <jeba.j@arus.co.in>
2026-09-11 10:31:36 +0530

    fix: testcase library

--- erpnext_australian_localisation/erpnext_australian_localisation/doctype/payment_batch/test_payment_batch.py
+++ erpnext_australian_localisation/erpnext_australian_localisation/doctype/payment_batch/test_payment_batch.py
@@ -2,14 +2,14 @@
 # See license.txt
 
 import frappe
-from frappe.tests import UnitTestCase
+from frappe.tests import FrappeTestCase
 
 from erpnext_australian_localisation.erpnext_australian_localisation.doctype.payment_batch.aba_file_generator import (
 	aba_account_field,
 )
 
 
-class TestPaymentBatch(UnitTestCase):
+class TestPaymentBatch(FrappeTestCase):
 	def test_aba_account_field_pads_to_nine(self):
 		self.assertEqual(aba_account_field("123456", "Supplier S-0001"), "   123456")
 		self.assertEqual(aba_account_field("123456789", "Supplier S-0001"), "123456789")
