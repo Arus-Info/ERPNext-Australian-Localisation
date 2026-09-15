2d722e34c461f961555835384e992677dbf365bf
user <thanisha.p@arus.co.in>
2026-09-15 11:04:12 +0530

    fix: description message

--- erpnext_australian_localisation/public/js/supplier.js
+++ erpnext_australian_localisation/public/js/supplier.js
@@ -43,7 +43,7 @@ function set_bank_account_description(frm) {
 		frm.set_df_property(
 			"bank_account_no",
 			"description",
-			__("10 digit bank account numbers will not be supported for abn file generation.")
+			__("10 digit bank account numbers will not be supported for aba file generation.")
 		);
 	} else {
 		frm.set_df_property("bank_account_no", "description", "");
