0c3abc4e031cf33a05aab944757a13d22f8b521e
Adhiyotvel <adhiyotvel.sv@arus.co.in>
2026-09-17 14:43:16 +0530

    feat: cache for connections

--- erpnext_australian_localisation/public/js/bank_account.js
+++ erpnext_australian_localisation/public/js/bank_account.js
@@ -26,14 +26,11 @@ frappe.ui.form.on("Bank Account", {
 });
 
 function fetch_provider_connections(frm) {
-	frappe.msgprint(__("Searching for bank connections..."));
-
 	frappe.call({
 		method: "erpnext_australian_localisation.integration.basiq.import_transaction.get_provider_connections",
+		freeze: true,
 
 		callback(r) {
-			frappe.hide_msgprint(true);
-
 			const connections = r.message || [];
 			if (!connections.length) {
 				frappe.msgprint(__("No bank connections found"));
@@ -95,6 +92,7 @@ function fetch_provider_accounts(frm, connection_id) {
 	frappe.call({
 		method: "erpnext_australian_localisation.integration.basiq.import_transaction.get_provider_accounts",
 		args: { connection_id },
+		freeze: true,
 
 		callback(r) {
 			frappe.hide_msgprint(true);
